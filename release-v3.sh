b5c61b3013092f8630ae9bf07246fa7195fb143b
Kirill Kirikov <kirill@bmybit.com>
2020-10-23 17:26:57 +0300

    Release charts

---
+++ release-v3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd charts
+helm3 package pipeline2
+helm3 package api
+helm3 package previewer
+
+cd ..
+helm3 repo index .
+
+# tag it
+git add -A
+git commit -m "Release charts"
+git push
